ea7f9cc43cd93106783d9ee1768234ebe0559968
shynn <tacoslama@gmail.com>
2024-09-04 21:46:11 +0200

    second commit

--- info1/tp1/resistance2.py
+++ info1/tp1/resistance2.py
@@ -17,6 +17,20 @@ if len(userColor0)>=3 :
     userColor1 = userColor0[0]
     userColor2 = userColor0[1]
     userColor3 = userColor0[2]
+else : 
+    print('erreur')
 
-resistance = ((dictionnaire[userColor1] * 10 ) + dictionnaire[userColor2])*(10**dictionnaire[userColor3])
-print(resistance)
+if userColor1 in dictionnaire and userColor2 in dictionnaire and userColor3 in dictionnaire :
+    resistance = ((dictionnaire[userColor1] * 10 ) + dictionnaire[userColor2])*(10**dictionnaire[userColor3])
+else : 
+    print("erreur2")
+
+if resistance>=1000 and resistance<1000000:
+    resistanceK = resistance/1000
+    print(resistanceK, "Kohm")
+elif resistance>=1000000:
+    resistanceM = resistance/1000000
+    print(resistanceM, "Mohm")
+else :
+    print(resistance, "ohm")
+
